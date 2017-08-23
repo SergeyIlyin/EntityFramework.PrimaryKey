f6ef096413133c84b400ada6ec33405279bf1838
Sergey Ilyin <silyinnur@gmail.com>
2017-08-23 12:52:13 +0500

    Find By PrimaryKey Extention

---
+++ EntityFrameworkCore.PrimaryKey/FindByKeyExtentions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace EntityFrameworkCore.PrimaryKey
+{
+    public static class FindByKeyExtentions
+    {
+        public static TEntity Find<TEntity>(this IQueryable<TEntity> source, PrimaryKeyDictionary<TEntity> primaryKeyDictionary) where TEntity : class
+        {
+            return source.FirstOrDefault(primaryKeyDictionary.MakeExpression());
+        }
+
+        static internal Expression<Func<TEntity, bool>> MakeExpression<TEntity>(this PrimaryKeyDictionary<TEntity> keyDictionary) where TEntity : class
+        {
+            var type = typeof(TEntity);
+            var pe = Expression.Parameter(type, "item");
+            Expression expression = keyDictionary.MakeExpression(pe);
+            if (expression == null)
+            {
+                return null;
+            }
+            var lambda = Expression.Lambda<Func<TEntity, bool>>(expression, pe);
+            return lambda;
+        }
+
+        static internal Expression MakeExpression<TEntity>(this PrimaryKeyDictionary<TEntity> keyDictionary, ParameterExpression parametr) where TEntity : class
+        {
+            if (!keyDictionary.Any())
+            {
+                return null;
+            }
+
+            Expression final = null;
+            Expression left = null;
+            foreach (var keyValue in keyDictionary)
+            {
+                var propertyExp = Expression.Property(parametr, keyValue.Key);
+                var valueExp = Expression.Constant(keyValue.Value);
+                var curExp = Expression.Equal(propertyExp, valueExp);
+                if (left == null)
+                {
+                    left = curExp;
+                }
+                else
+                {
+                    var rigth = curExp;
+                    final = Expression.AndAlso(left, rigth);
+                    left = final;
+                }
+            }
+            return final ?? left;
+        }
+    }
+}
---
+++ TestsCore/FindByKeyExtentions_Tests.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EntityFrameworkCore.PrimaryKey.TestsCore
+{
+ public    class SingleColumnFind:TestBase
+    {
+        A e = new A { Id = 42L };
+        A e1 = new A { Id = 43L };
+        [Fact]
+        public void FindTest()
+        {
+            var source = new List<A> { e, e1 }.AsQueryable();
+            var key =e.GetPrimaryKey();
+            var finded = source.Find(key);
+            Assert.Equal(e, finded);
+        }
+    }
+}
